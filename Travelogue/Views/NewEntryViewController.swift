ad650e55600f2de77768b79651aa5b6d10b8fc48
Livvy Beggs <obeggs11@gmail.com>
2020-12-09 08:49:25 -0600

    Created view controllers
    Added view controllers to the views folder

---
+++ Travelogue/Views/NewEntryViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NewEntryViewController.swift
+//  Travelogue
+//
+//  Created by obbrbt on 12/9/20.
+//
+
+import UIKit
+
+class NewEntryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
