fa02bf0ad1afdc7091c8a284ed7c0b6646244f73
Livvy Beggs <obeggs11@gmail.com>
2020-12-09 08:45:16 -0600

    Initial Commit

---
+++ Travelogue/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Travelogue
+//
+//  Created by obbrbt on 12/9/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
