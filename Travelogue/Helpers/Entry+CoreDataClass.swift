97ed7e17c83e93f2981390cfc5a23c07dd839e13
Livvy Beggs <obeggs11@gmail.com>
2020-12-09 09:22:00 -0600

    Added initalizers and update
    Added convenience init to EntryCoreDataClass, added update function, added conversion from Binary Data to UIImage.

--- Travelogue/Helpers/Entry+CoreDataClass.swift
+++ Travelogue/Helpers/Entry+CoreDataClass.swift
@@ -6,10 +6,57 @@
 //
 //
 
-import Foundation
+import UIKit
 import CoreData
 
 @objc(Entry)
 public class Entry: NSManagedObject {
-
+    
+    func convertToData(image: UIImage?) -> Data?
+    {
+        return image?.pngData() as Data?
+    }
+    
+    var photo: UIImage?
+    {
+        get
+        {
+            if let imageData = rawImage
+            {
+                return UIImage(data: imageData)
+            }
+            return nil
+        } set
+        {
+            rawImage = convertToData(image: newValue)
+            if let imageNewValue = newValue
+            {
+                rawImage = convertToData(image: imageNewValue)
+            }
+        }
+    }
+    
+    convenience init?(title: String?, body: String?, image: UIImage?)
+    {
+        let appDelegate = UIApplication.shared.delegate as? AppDelegate
+        guard let managedContext = appDelegate?.persistentContainer.viewContext,
+              let title = title, title != "" else
+        {
+            return nil
+        }
+        
+        self.init(entity: Entry.entity(), insertInto: managedContext)
+        self.title = title
+        self.body = body
+        self.date = Date(timeIntervalSinceNow: 0)
+        self.rawImage = convertToData(image: image)
+    }
+    
+    func update(title: String?, body: String?, image: UIImage?)
+    {
+        self.title = title
+        self.body = body
+        self.date = Date(timeIntervalSinceNow: 0)
+        self.rawImage = convertToData(image: image)
+    }
 }
